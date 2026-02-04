07ea7f281951dc058e5868f32c3516daea9d949c
Martiens Kropff <martiensk@gmail.com>
2026-02-04 11:58:44 +0000

    feat: add color picker component with hex color support and integrate into tag components

--- src/components/tag/tag-button.tsx
+++ src/components/tag/tag-button.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import classNames from 'classnames';
 import {useTranslation} from 'react-i18next';
 import {IconChevronDown} from '@tabler/icons';
-import {MenuButton} from '../control/menu-button';
-import {colors, Color} from '../../util/color';
+import {Color, isValidHexColor} from '../../util/color';
+import {ColorPicker} from '../control/color-picker';
 import './tag-button.css';
 
 export interface TagButtonProps {
@@ -16,30 +16,69 @@ export interface TagButtonProps {
 
 export const TagButton: React.FC<TagButtonProps> = props => {
 	const {t} = useTranslation();
+	const [open, setOpen] = React.useState(false);
+	const menuRef = React.useRef<HTMLDivElement>(null);
+
+	function hexToRgb(hex: string): string {
+		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		if (!result) {
+			return '0, 0, 0';
+		}
+		return `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
+	}
+
+	React.useEffect(() => {
+		function handleClickOutside(e: MouseEvent) {
+			if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+				setOpen(false);
+			}
+		}
+
+		if (open) {
+			document.addEventListener('mousedown', handleClickOutside);
+			return () => document.removeEventListener('mousedown', handleClickOutside);
+		}
+	}, [open]);
+
+	const isHex = isValidHexColor(props.color as string);
+	const style = isHex ? {'--hex-rgb': hexToRgb(props.color as string)} as React.CSSProperties : undefined;
 
 	return (
-		<span className={classNames('tag-button', `color-${props.color}`)}>
-			<MenuButton
+		<div 
+			ref={menuRef}
+			className={classNames('tag-button', {
+				'tag-button--hex': isValidHexColor(props.color as string),
+				[`color-${props.color}`]: !isValidHexColor(props.color as string),
+				'tag-button--open': open
+			})}
+			style={style}
+		>
+			<button
+				className="tag-button__trigger"
 				disabled={props.disabled}
-				icon={<IconChevronDown />}
-				iconPosition="end"
-				items={[
-					...colors.map(color => ({
-						checkable: true,
-						checked: color === 'none' ? !props.color : color === props.color,
-						label: t(`colors.${color}`),
-						onClick: () => props.onChangeColor(color)
-					})),
-					{
-						separator: true
-					},
-					{
-						label: t('common.remove'),
-						onClick: props.onRemove
-					}
-				]}
-				label={props.name}
-			/>
-		</span>
+				onClick={() => setOpen(!open)}
+				type="button"
+			>
+				{props.name}
+				<IconChevronDown size={16} />
+			</button>
+
+			{open && (
+				<div className="tag-button__dropdown">
+					<ColorPicker
+						color={props.color}
+						onChangeColor={props.onChangeColor}
+					/>
+					<div className="tag-button__dropdown-separator" />
+					<button
+						className="tag-button__dropdown-item tag-button__dropdown-item--remove"
+						onClick={props.onRemove}
+						type="button"
+					>
+						{t('common.remove')}
+					</button>
+				</div>
+			)}
+		</div>
 	);
 };
--- src/components/tag/tag-stripe.tsx
+++ src/components/tag/tag-stripe.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {TagColors} from '../../store/stories';
+import {isValidHexColor} from '../../util/color';
 import './tag-stripe.css';
 
 export interface TagStripeProps {
@@ -12,13 +13,20 @@ export const TagStripe: React.FC<TagStripeProps> = React.memo(props => {
 		<div className="tag-stripe">
 			{props.tags
 				.filter(tag => tag in props.tagColors)
-				.map(tag => (
-					<span
-						className={`color-${props.tagColors[tag]}`}
-						key={tag}
-						title={tag}
-					/>
-				))}
+				.map(tag => {
+					const color = props.tagColors[tag];
+					const isHex = isValidHexColor(color as string);
+					const style = isHex ? {backgroundColor: color} : undefined;
+
+					return (
+						<span
+							className={isHex ? 'color-hex' : `color-${color}`}
+							key={tag}
+							title={tag}
+							style={style}
+						/>
+					);
+				})}
 		</div>
 	);
 });
